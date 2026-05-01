cf903cda202369b326163b53fbfc0b0d58484577
4aadhi6 <adithyanr350@gmail.com>
2026-05-01 20:26:52 +0530

    Add useRef import to BookingStatus component

--- src/pages/BookingStatus.tsx
+++ src/pages/BookingStatus.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef} from "react";
 import { useParams, Link } from "react-router-dom";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
